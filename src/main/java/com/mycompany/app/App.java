9d5f8ab56613b35a2ce6884c0eed23ea671978f4
Sk <Sk@gmail.com>
2021-11-15 14:59:11 +0000

    reverted

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -6,7 +6,7 @@ package com.mycompany.app;
 public class App
 {
 
-    private final String message = "Hello World! Swagat nahi kroge hamara";
+    private final String message = "Hello World!";
 
     public App() {}
 
